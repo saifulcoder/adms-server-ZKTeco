410dfc22c0e136dd33aa5cef3f0a7ba548764c0d
saiful anwar <saiful.coder@gmail.com>
2024-07-24 23:40:48 +0700

    Update Handler.php

--- app/Exceptions/Handler.php
+++ app/Exceptions/Handler.php
@@ -3,9 +3,9 @@
 namespace App\Exceptions;
 
 use Illuminate\Foundation\Exceptions\Handler as ExceptionHandler;
-use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
-use Illuminate\Support\Facades\Log;
 use Throwable;
+use Illuminate\Support\Facades\Log;
+use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
 
 class Handler extends ExceptionHandler
 {
@@ -26,27 +26,11 @@ class Handler extends ExceptionHandler
     public function register(): void
     {
         $this->reportable(function (Throwable $e) {
-            if ($e instanceof NotFoundHttpException) {
-                Log::error('404 Error: ' . $e->getMessage(), [
-                    'url' => request()->url(),
-                    'ip' => request()->ip(),
-                    'user_agent' => request()->header('User-Agent'),
-                ]);
-            }
+            //
         });
-    }
 
-    /**
-     * Render an exception into an HTTP response.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  \Throwable  $e
-     * @return \Symfony\Component\HttpFoundation\Response
-     *
-     * @throws \Throwable
-     */
-    public function render($request, Throwable $e)
-    {
-        return parent::render($request, $e);
+        $this->renderable(function (NotFoundHttpException $e, $request) {
+            Log::channel('daily')->error('404 Not Found: ' . $request->url());
+        });
     }
 }
