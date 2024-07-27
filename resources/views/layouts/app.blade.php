5033a3ac3d839ff46a8a83a7acc5af34c333c6db
saiful anwar <saiful.coder@gmail.com>
2024-07-27 23:39:51 +0700

    Update app.blade.php

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -3,17 +3,45 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>My App</title>
-    <title>Step-by-Step Tutorial: Implementing Yajra Datatables in Laravel 10 - CodeAndDeploy.com</title>
+    <title>ADMS Server</title>
     <meta name="csrf-token" content="{{ csrf_token() }}">
     <link href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/5.0.1/css/bootstrap.min.css" rel="stylesheet">
     <link href="https://cdn.datatables.net/1.11.4/css/dataTables.bootstrap5.min.css" rel="stylesheet">
+    <style>
+        @media (max-width: 991.98px) {
+            .navbar-collapse {
+                position: fixed;
+                top: 56px; /* Adjust this value based on your navbar height */
+                left: -100%;
+                padding-left: 15px;
+                padding-right: 15px;
+                padding-bottom: 15px;
+                width: 75%;
+                height: 100%;
+                background-color: #f8f9fa;
+                transition: all 0.3s ease-in-out;
+                z-index: 1000;
+            }
+
+            .navbar-collapse.show {
+                left: 0;
+            }
+
+            body.menu-open {
+                overflow: hidden;
+            }
+
+            .navbar-toggler {
+                z-index: 1001;
+            }
+        }
+    </style>
 </head>
 <body>
     <nav class="navbar navbar-expand-lg navbar-light bg-light">
         <div class="container">
             <a class="navbar-brand" href="#">ADMS Server</a>
-            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
                 <span class="navbar-toggler-icon"></span>
             </button>
             <div class="collapse navbar-collapse" id="navbarNav">
@@ -27,10 +55,11 @@
                     <li class="nav-item">
                         <a class="nav-link" href="{{ route('devices.FingerLog') }}">Finger Log</a>
                     </li>
-
                 </ul>
             </div>
-            <span>{{ now() }}</span>
+            <span class="navbar-text d-none d-lg-block">
+                {{ now() }}
+            </span>
         </div>
     </nav>
 
@@ -43,8 +72,19 @@
     <script src="https://cdn.datatables.net/1.11.4/js/jquery.dataTables.min.js"></script>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
     <script src="https://cdn.datatables.net/1.11.4/js/dataTables.bootstrap5.min.js"></script>
+    <script>
+        $(document).ready(function() {
+            $('.navbar-toggler').on('click', function() {
+                $('body').toggleClass('menu-open');
+            });
+
+            $('.nav-link').on('click', function() {
+                if ($(window).width() < 992) {
+                    $('.navbar-collapse').removeClass('show');
+                    $('body').removeClass('menu-open');
+                }
+            });
+        });
+    </script>
 </body>
-
-
-
 </html>
