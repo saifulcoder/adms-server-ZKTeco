6982b841667cb55d2e48eac57b667c29f11e540f
saiful anwar <saiful.coder@gmail.com>
2024-07-29 09:30:56 +0700

    fixed

--- app/Http/Controllers/iclockController.php
+++ app/Http/Controllers/iclockController.php
@@ -1,9 +1,11 @@
 <?php
 
 namespace App\Http\Controllers;
-
+use App\Models\Attendance;
 use Illuminate\Http\Request;
+use Carbon\Carbon;
 use Illuminate\Support\Facades\DB;
+
 
 class iclockController extends Controller
 {
@@ -16,8 +18,8 @@ class iclockController extends Controller
     // handshake
     public function handshake(Request $request)
     {
-        $data['url'] = \Request::getRequestUri();
-        $data["data"] = json_encode($request->all());
+        $data['url'] = $request->all();
+        $data["data"] = json_encode($request->getContent());
         $data["sn"] = $request->SN;
         $data["option"] = $request->option;
         DB::table('device_log')->insert($data);
@@ -27,21 +29,21 @@ class iclockController extends Controller
             ['no_sn' => $request->SN],
             ['online' => now()]
         );
-            $r = "GET OPTION FROM: ".$request->SN." \n\r
-            Stamp=9999 \n\r
-            OpStamp=".strtotime('now')." \n\r
-            ErrorDelay=60 \n\r
-            Delay=30 \n\r
-            ResLogDay=18250 \n\r
-            ResLogDelCount=10000 \n\r
-            ResLogCount=50000 \n\r
-            TransTimes=00:00;14:05 \n\r
-            TransInterval=1 \n\r
-            TransFlag=1111000000 \n\r
-            Realtime=1 \n\r
-            Encrypt=0";
-            $r = trim(preg_replace('/\t+/', '', $r));
-        // $r = "GET OPTION FROM:%s{$request->SN}\nStamp=1565089939\nOpStamp=1565089939\nErrorDelay=30\nDelay=10\nTransTimes=00:00;14:05\nTransInterval=1\nTransFlag=1111000000\nTimeZone=7\nRealtime=1\nEncrypt=0\n";
+            // $r = "GET OPTION FROM: ".$request->SN." \n\r
+            // Stamp=9999 \n\r
+            // OpStamp=".strtotime('now')." \n\r
+            // ErrorDelay=60 \n\r
+            // Delay=30 \n\r
+            // ResLogDay=18250 \n\r
+            // ResLogDelCount=10000 \n\r
+            // ResLogCount=50000 \n\r
+            // TransTimes=00:00;14:05 \n\r
+            // TransInterval=1 \n\r
+            // TransFlag=1111000000 \n\r
+            // Realtime=1 \n\r
+            // Encrypt=0";
+            // $r = trim(preg_replace('/\t+/', '', $r));
+        $r = "GET OPTION FROM:%s{$request->SN}\nStamp=".strtotime('now')."\nOpStamp=1565089939\nErrorDelay=30\nDelay=10\nTransTimes=00:00;14:05\nTransInterval=1\nTransFlag=1111000000\nTimeZone=7\nRealtime=1\nEncrypt=0\n";
 
         return $r;
     }
@@ -53,10 +55,28 @@ class iclockController extends Controller
     {   
         
         try {
-        $content['data'] = json_encode($request->all());;
+        $content['url'] = json_encode($request->all());
+        $content['data'] = json_encode($request->getContent());;
         DB::table('finger_log')->insert($content);
-        $arr = preg_split('/\\r\\n|\\r|,|\\n/', $content['data']);
+        //$arr = preg_split('/\\r\\n|\\r|,|\\n/', $content['data']);
+        $arr = explode("\n", $content['data']);
         $tot = count($arr);
+
+        foreach ($arr as $line) {
+            $data = preg_split('/\s+/', trim($line));
+
+            if (count($data) == 7) {
+                Attendance::create([
+                    'employee_id' => $data[0],
+                    'timestamp' => Carbon::createFromFormat('Y-m-d H:i:s', $data[1] . ' ' . $data[2]),
+                    'status1' => (bool) $data[3],
+                    'status2' => (bool) $data[4],
+                    'status3' => (bool) $data[5],
+                    'status4' => (bool) $data[6],
+                    'status5' => (bool) $data[7], // Menggunakan data[6] karena data asli hanya memiliki 6 kolom
+                ]);
+            }
+        }
 
             return "OK: ".$tot;
         } catch (Throwable $e) {
---
+++ database/migrations/2024_07_29_022209_create_attendances_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+     Schema::create('attendances', function (Blueprint $table) {
+            $table->id();
+            $table->integer('employee_id');
+            $table->dateTime('timestamp');
+            $table->boolean('status1');
+            $table->boolean('status2');
+            $table->boolean('status3');
+            $table->boolean('status4');
+            $table->boolean('status5');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('attendances');
+    }
+};
